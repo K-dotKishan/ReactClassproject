b268e0d4d91e2fde7a29a3b559f85ecbd955a1a6
k-dotsingh <kishansingh2882004@gmail.com>
2026-02-18 11:42:04 +0530

    added console chechs for errrors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,6 +18,7 @@ export default function Home() {
         try {
             if (loading) return
             const currentToken = nextPageToken
+            console.log(currentToken)
             if (!currentToken) return
 
             const response = await fetch(
